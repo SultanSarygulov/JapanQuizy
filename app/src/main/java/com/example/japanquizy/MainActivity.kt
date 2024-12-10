0ab2cd55f6117a35be334b77924f6309e1fd05b0
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2024-12-11 02:10:44 +0600

    Created Main Activity

--- app/src/main/java/com/example/japanquizy/MainActivity.kt
+++ app/src/main/java/com/example/japanquizy/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.japanquizy
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.japanquizy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val buttons = listOf(
+            binding.button1,
+            binding.button2,
+            binding.button3,
+            binding.button4
+        )
+
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                when (button.id) {
+                    binding.button1.id -> {
+                        if (button.currentTextColor == Color.GREEN) {
+
+                            resetButtons(buttons)
+                        } else {
+                            resetButtons(buttons)
+                            button.setBackgroundColor(Color.GREEN)
+                            button.setTextColor(Color.WHITE)
+                        }
+                    }
+                    else -> {
+//                        resetButtons(buttons)
+                        button.setBackgroundColor(Color.RED)
+                        button.setTextColor(Color.WHITE)
+
+                        binding.button1.setBackgroundColor(Color.GREEN)
+                        binding.button1.setTextColor(Color.WHITE)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun resetButtons(buttons: List<Button>) {
+        buttons.forEach { button ->
+            i += 1
+            Log.d("chura", "resetButtons $i")
+            button.setBackgroundColor(Color.LTGRAY)
+            button.setTextColor(Color.BLACK)
+
+        }
+
     }
 }
+
+var i: Int = 0
